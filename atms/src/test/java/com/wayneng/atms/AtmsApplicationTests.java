81ed63985b427343386f8081e41e56357d325fc6
Wayne Ng <ngzuwayne@gmail.com>
2026-04-14 14:55:06 +0800

    First commit

---
+++ atms/src/test/java/com/wayneng/atms/AtmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wayneng.atms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
