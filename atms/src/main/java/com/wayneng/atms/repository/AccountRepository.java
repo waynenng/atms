9950ad8a98a15c84f0ec5dbd8af4410d1515139e
Wayne Ng <ngzuwayne@gmail.com>
2026-04-16 10:30:47 +0800

    Defined the Account repository

---
+++ atms/src/main/java/com/wayneng/atms/repository/AccountRepository.java
@@ -0,0 +1,19 @@
+package com.wayneng.atms.repository;
+
+import com.wayneng.atms.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.List;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByAccountNumber(String accountNumber);
+
+    Optional<Account> findByCardsCardNumber(String cardNumber);
+
+    List<Account> findByCustomerId(Long customerId);
+
+    List<Account> findByAccountType(String accountType);
+
+    List<Account> findByStatus(String status);
+}
