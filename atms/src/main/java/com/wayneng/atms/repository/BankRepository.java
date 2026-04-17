f4048ddd4affbd1abca1b68e319b7a9606e5104d
Wayne Ng <ngzuwayne@gmail.com>
2026-04-17 16:14:25 +0800

    Cleared the Bank repository to reflect the reality of this application

--- atms/src/main/java/com/wayneng/atms/repository/BankRepository.java
+++ atms/src/main/java/com/wayneng/atms/repository/BankRepository.java
@@ -2,11 +2,8 @@ package com.wayneng.atms.repository;
 
 import com.wayneng.atms.model.Bank;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 public interface BankRepository extends JpaRepository<Bank, Long> {
 
-    Optional<Bank> findByName(String name);
 
-    Optional<Bank> findByBankCode(String bankCode);
 }
