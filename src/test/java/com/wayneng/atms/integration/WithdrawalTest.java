aac73c866a9f898e99b6a752bc793afbcbb28f11
Wayne Ng <ngzuwayne@gmail.com>
2026-04-25 16:57:15 +0800

    Minor edit

--- src/test/java/com/wayneng/atms/integration/WithdrawalTest.java
+++ src/test/java/com/wayneng/atms/integration/WithdrawalTest.java
@@ -8,10 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
-
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
