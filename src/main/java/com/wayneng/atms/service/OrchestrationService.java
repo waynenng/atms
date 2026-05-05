690cfa448ea19e6e6b5debdb032dd2f08dd5d1ab
Wayne Ng <ngzuwayne@gmail.com>
2026-05-05 14:32:57 +0800

    Got rid of the orchestration service

--- src/main/java/com/wayneng/atms/service/OrchestrationService.java
+++
@@ -1,8 +0,0 @@
-package com.wayneng.atms.service;
-
-import java.math.BigDecimal;
-
-public interface OrchestrationService {
-
-    void orchestrate(String operationType, String cardNumber, String pin, String atmCode, BigDecimal amount);
-}
